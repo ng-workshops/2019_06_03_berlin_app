c39fc638f91d78b325dc7d6ed4ec72eb95e36321
Andreas Krummsdorf <andreas.krummsdorf@doksafe.de>
2019-06-05 21:16:20 +0200

    Update ngx-translate example

--- src/app/home/impressum2/impressum2.component.ts
+++ src/app/home/impressum2/impressum2.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-impressum2',
   templateUrl: './impressum2.component.html',
   styleUrls: ['./impressum2.component.scss']
 })
-export class Impressum2Component {
+export class Impressum2Component implements OnInit {
   private today = new Date();
+  locale: string;
 
   lastUpdatedAt = new Date(2019, 5, 3, 10, 10, 10);
   days =
@@ -19,10 +20,15 @@ export class Impressum2Component {
   };
 
   fromCode$ = this.translate.onLangChange.pipe(
+    tap(event => (this.locale = event.lang)),
     switchMap(() => this.translate.get('impressum.title'))
   );
 
   constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.locale = this.translate.currentLang;
+  }
 
   useLanguage(language: string) {
     this.translate.use(language);
